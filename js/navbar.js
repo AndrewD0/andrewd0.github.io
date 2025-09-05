28707683dea3e8f6fe49ddb61d8a160a8d5f1ec6
Andrew Du <andrew.selkirk17@gmail.com>
2025-09-04 20:58:28 -0700

    Javascript

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+document.body.insertAdjacentHTML('afterbegin', `
+  <nav class="navbar">
+    <div class="logo">
+      <a href="/">
+        <span class="first">andrew's</span>
+        <span class="second">desk</span>
+      </a>
+    </div>
+
+    <div class="links">
+      <li><a href="projects.html">PROJECTS</a></li>
+      <li><a href="assets/resume/Andrew_s_Official_Resume.pdf" target="_blank">RESUME</a></li>
+    </div>
+  </nav>
+`);
